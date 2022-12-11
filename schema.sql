6a720feba484220fdb8a4feac3f82e9ea607199b
ykpythemind <yukibukiyou@gmail.com>
2022-12-11 16:29:56 +0900

    fix

--- schema.sql
+++ schema.sql
@@ -1,14 +1,13 @@
 CREATE TABLE mergeSchedules (
-  id INT NOT NULL,
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
   installationId INT NOT NULL,
 
   -- 非正規化
   repositoryOwner VARCHAR(255) NOT NULL,
   repositoryName VARCHAR(255) NOT NULL,
-  pullRequestNumber INT NOT NULL,
+  pullRequestNumber INTEGER NOT NULL,
 
-  willMergeAt DATETIME NOT NULL,
-  PRIMARY KEY (`id`)
+  willMergeAt DATETIME NOT NULL
 );
 
 CREATE INDEX willMergeAtIndex on mergeSchedules(willMergeAt);
